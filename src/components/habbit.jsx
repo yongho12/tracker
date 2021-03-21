47d1b9fad5ccd6651071a9416d341ad01faca45b
Yongho Kim <winthisgame12@gmail.com>
2021-03-20 23:13:58 -0600

    add setState in Habbit.jsx

--- src/components/habbit.jsx
+++ src/components/habbit.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
 class Habbit extends Component {
+    state = {
+        count:0,
+    }
+
+    handleIncrememt = ()=>{
+        this.setState( {count:this.state.count + 1} )
+    }
+
+    handleDecrememt = ()=>{
+        const count = this.state.count -1;
+        this.setState({ count: count<0 ? 0:count} )
+    }
+    
+    
     render() {
         return (
             <>    
                 <span className="habit-name">Reading</span>
-                <span className="habit-count">8</span>
-                <button className="habit-button habit-increase">
+                <span className="habit-count">{this.state.count}</span>
+                <button className="habit-button habit-increase" onClick={this.handleIncrememt}>
                     <i className="fas fa-plus-square"></i>
                 </button >
-                 <button className="habit-button habit-increase">
+                 <button className="habit-button habit-increase" onClick={this.handleDecrememt}>
                     <i className="fas fa-minus-square"></i>
                 </button >
                 <button className="habit-button habit-delete">
